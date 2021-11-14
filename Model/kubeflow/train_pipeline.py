d3232bb87ca6e62dd026af15840a3ece4b4d9fef
Ryan Sinnet <rsinnet@misorobotics.com>
2021-11-13 17:45:13 -0800

    RAD-40: WIP Update pipeline for Vertex AI
    Update to kfp v2 to be compatible with Vertex AI.

--- Model/kubeflow/train_pipeline.py
+++ Model/kubeflow/train_pipeline.py
@@ -1,36 +1,37 @@
-import kfp.dsl as dsl
+import kfp.v2.dsl as dsl
 import kfp.gcp as gcp
 
 
+@dsl.component()
 def train_op(
-    *,
-    gcs_bucket,
-    pose_estimation_gcs_path,
-    log_dir,
-    docker_image,
-    epochs,
-    memory_limit,
-    num_gpu,
-    gpu_type,
-    checkpoint_file=None,
+    gcs_bucket: str,
+    pose_estimation_gcs_path: str,
+    log_dir: str,
+    docker_image: str,
+    epochs: int,
+    memory_limit: str,
+    num_gpu: int,
+    gpu_type: str,
+    checkpoint_file: str = None,
 ):
-    """ Create a Kubeflow ContainerOp to train an estimator on the single cube dataset.
-    
+    """Create a Kubeflow ContainerOp to train an estimator on the single cube dataset.
+
     Args:
         gcs_bucket: GCS Bucket where the datasets are located
         pose_estimation_gcs_path: Path inside the gcp bucket where the datasets are located
         log_dir: path to save the Tensorboard event files.
         docker_image (str): Docker image registry URI.
-        epochs: number of epochs on which we want ot train the model
+        epochs: number of epochs on which we want to train the model
         memory_limit (str): Set memory limit for this operator. For simplicity,
             we set memory_request = memory_limit.
         num_gpu (int): Set the number of GPU for this operator
         gpu_type (str): Set the type of GPU
+
     Returns:
         kfp.dsl.ContainerOp: Represents an op implemented by a container image
             to train an estimator.
-    """
 
+    """
     command = ["python", "-m", "pose_estimation.cli"]
     arguments = [
         "train",
@@ -42,25 +43,28 @@ def train_op(
         f"--log-dir-system={log_dir}",
     ]
 
-    train = dsl.ContainerOp(
-        name="train", image=docker_image, command=command, arguments=arguments,
+    import subprocess
+
+    process = subprocess.Popen(
+        command + arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
-    # GPU
-    train.set_gpu_limit(num_gpu)
-    train.add_node_selector_constraint(
-        "cloud.google.com/gke-accelerator", gpu_type
-    )
+    process.communicate()
 
-    train.set_memory_request(memory_limit)
-    train.set_memory_limit(memory_limit)
+    # # GPU
+    # train.set_gpu_limit(num_gpu)
+    # train.add_node_selector_constraint(
+    #     "cloud.google.com/gke-accelerator", gpu_type
+    # )
 
-    train.apply(gcp.use_gcp_secret("user-gcp-sa"))
+    # train.set_memory_request(memory_limit)
+    # train.set_memory_limit(memory_limit)
 
-    return train
+    # train.apply(gcp.use_gcp_secret("user-gcp-sa"))
 
 
 @dsl.pipeline(
-    name="train pipeline", description="train the model using kubeflow pipeline"
+    name="train-pipeline",
+    description="train the model using kubeflow pipeline",
 )
 def train_pipeline_single_cube(
     docker_image: str = "",
@@ -86,12 +90,11 @@ def train_pipeline_single_cube(
         gpu_type=gpu_type,
     )
 
-    
-
 
 if __name__ == "__main__":
-    import kfp.compiler as compiler
+    from kfp.v2 import compiler
 
     compiler.Compiler().compile(
-        train_pipeline_single_cube, __file__ + ".tar.gz"
+        pipeline_func=train_pipeline_single_cube,
+        package_path=f"{__file__}.json",
     )
